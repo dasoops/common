1f629af9ebacbde9130a10b7ad44422be1b4a0aa
Dasoops <DasoopsNicole@gmail.com>
2023-03-08 14:12:31 +0800

    feat: 添加项目内部异常支持

---
+++ src/main/java/com/dasoops/common/exception/ProjectException.kt
@@ -0,0 +1,36 @@
+package com.dasoops.common.exception
+
+import com.dasoops.common.entity.enums.exception.ExceptionEnum
+import com.dasoops.common.entity.enums.exception.IExceptionEnum
+
+/**
+ * 项目通用异常枚举(200xx)
+ * @title: RuntimeDataExceptionEnum
+ * @classPath com.herdsman.wgts.api.originalData.RuntimeDataExceptionEnum
+ * @author DasoopsNicole@Gmail.com
+ * @date 2023/03/04
+ * @version 1.0.0
+ * @see [ProjectExceptionEnum]
+ */
+enum class ProjectExceptionEnum(private val msg: String) : IExceptionEnum {
+    NO_AUTH("没有权限这样操作"),
+    NO_RECORD("没有查询到数据"),
+    ;
+
+    override fun getCode(): Int {
+        return 20000 + ordinal
+    }
+
+    override fun getMsg(): String {
+        return msg
+    }
+
+    override fun getException(): ProjectException {
+        return ProjectException(this)
+    }
+}
+
+class ProjectException(exceptionEnum: ProjectExceptionEnum) : CustomException(exceptionEnum)
+
+object UnexpectedException : CustomException(ExceptionEnum.UN_EXPECTED)
+object NoRecordException : CustomException(ExceptionEnum.NO_RECORD)
