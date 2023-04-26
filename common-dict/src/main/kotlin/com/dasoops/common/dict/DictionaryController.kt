340e3d78ba4795915e8c10a4b8b17e1ff2ef1d47
Dasoops <DasoopsNicole@gmail.com>
2023-04-26 13:25:57 +0800

    fix: 修复common-dict接口Result错误

--- common-dict/src/main/kotlin/com/dasoops/common/dict/DictionaryController.kt
+++ common-dict/src/main/kotlin/com/dasoops/common/dict/DictionaryController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import com.dasoops.common.entity.result.Result
 
 /**
  * 字典控制器
